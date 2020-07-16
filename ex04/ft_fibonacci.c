3b4848f3034a33f947f5b3108a4e1f651b3df6fc
Nomasam Mbanjwa <nombanjw@c4r16s6.wethinkcode.co.za>
2020-07-16 14:32:29 +0200

    ex04

---
+++ ex04/ft_fibonacci.c
@@ -0,0 +1,14 @@
+#include<unistd.h>
+#include <stdio.h>
+int ft_fibonacci(int index)
+{
+if (index==0){
+	return (0);}
+else if(index==1){
+	return (1);}
+else if (index>1){
+return (ft_fibonacci(index-2)+ft_fibonacci(index-1));}
+}
+/*int main(){
+printf("%d" , ft_fibonacci(7));
+}*/
