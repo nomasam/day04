f07e207026d67862739b82ad3c6f7f3552cc6f43
Nomasam Mbanjwa <nombanjw@c4r16s6.wethinkcode.co.za>
2020-07-16 13:17:32 +0200

    ex01

--- ex01/ft_recursive_factorial.c
+++ ex01/ft_recursive_factorial.c
@@ -3,35 +3,14 @@
 
 
 
-int ft_iterative_factorial(int nb)
+
+int 	ft_recursive_factorial(int nb)
 {
-  
-int fact;
-int i;
-
-fact = 1;
-i = 1;
-  
-
-  
-  if (nb < 0 )
-
-  {
-    return (0);
-  }
-  if (nb <= 1)
-  {
-    fact = 1;
-    return (fact);
-  }
-  
-  else
-  {
-    while(i <= nb)
-    {
-      fact = (fact * i);
-      i++;
-    }
-    return (fact);
-  }
+		
+		if(nb > 1)
+		{
+			nb = nb * ft_recursive_factorial(nb -1);
+		}
+		return (nb);
+ 
 }
